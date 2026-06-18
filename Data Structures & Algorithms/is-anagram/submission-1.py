6385b3045c657946848bffc8bef01cc19e2ea28f
NIMITMAROO <nimitmaroo965@gmail.com>
2026-06-18 15:12:55 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        flag = False
+        s1 = s
+        t1 = t
+        i = 0
+
+        if len(s) != len(t):
+            return flag
+        
+        else:
+
+            while i != len(s):
+                
+                if s[i] in t:
+                    t = t.replace(s[i], "", 1)
+                i += 1
+            
+            if t == "":
+                flag = True
+            return flag
